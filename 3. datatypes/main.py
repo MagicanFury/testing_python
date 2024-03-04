5e619cebe8a1bc72d28dd18d8b43899ef0ede22c
Ivan Auda <ivan.auda@hva.nl>
2024-03-04 18:06:25 +0100

    Added type checking in example 3

--- 3. datatypes/main.py
+++ 3. datatypes/main.py
@@ -27,3 +27,16 @@ hello = "hello"
 world = "world"
 helloworld = hello + " " + world
 print(helloworld)
+
+# change this code
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+# testing code
+if mystring == "hello":
+  print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+  print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+  print("Integer: %d" % myint)
