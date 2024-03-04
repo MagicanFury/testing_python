fe26c24d8204ad39b45b1e09982e816fdd1b2dfd
Ivan Auda <ivan.auda@hva.nl>
2024-03-04 18:04:49 +0100

    Created example 3: datatypes

---
+++ 3. datatypes/main.py
@@ -0,0 +1,29 @@
+# Integer
+myint = 7
+print(myint)
+
+# Float
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+# Addition
+one = 1
+two = 2
+three = one + two
+print(three)
+
+# String
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
+mystring = "Don't worry about apostrophes"
+print(mystring)
+
+# Concat
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
