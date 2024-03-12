ab71f3eab296b54ecb29e3de83d4ac554345c112
Ivan Auda <ivan.auda@hva.nl>
2024-03-13 00:47:25 +0100

    Created example 4: functions

---
+++ 4. functions/main.py
@@ -0,0 +1,13 @@
+
+def print_hello_world():
+  print("Hello World")
+
+def sum_two_numbers(a, b):
+  return a + b
+
+def my_function_with_args(username, greeting):
+  print("Hello, %s, From My Function!, I wish you %s"%(username, greeting))
+
+print_hello_world()
+print(sum_two_numbers(10.0, 5))
+my_function_with_args("John Doe", "a great year!")
