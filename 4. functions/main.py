71df34fc0e827fce29d0608921f4b7169477a687
Ivan Auda <ivan.auda@hva.nl>
2024-03-18 23:38:47 +0100

    Implemented pylint for example 4

--- 4. functions/main.py
+++ 4. functions/main.py
@@ -1,12 +1,16 @@
+"""Module showcasing some basic functions."""
 
 def print_hello_world():
-  print("Hello World")
+    """Function printing hello world."""
+    print("Hello World")
 
 def sum_two_numbers(a, b):
-  return a + b
+    """Function returning the sub of two numbers."""
+    return a + b
 
 def my_function_with_args(username, greeting):
-  print("Hello, %s, From My Function!, I wish you %s"%(username, greeting))
+    """Function printing a greeting message."""
+    print(f"Hello, {username}, From My Function!, I wish you {greeting}")
 
 print_hello_world()
 print(sum_two_numbers(10.0, 5))
